26b5229622e98f6829b205bad19da4fdba55c97e
akibhasan50 <tonuakibhasan@gmail.com>
2022-03-29 00:26:15 +0600

    dynamic pie chart

--- src/components/Charts/Pie3D.js
+++ src/components/Charts/Pie3D.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import FusionCharts from "fusioncharts";
+import Charts from "fusioncharts/fusioncharts.charts";
+import ReactFC from "react-fusioncharts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
 
-const Pie3D = () => {
-  return <div>chart</div>;
+ReactFC.fcRoot(FusionCharts, Charts, FusionTheme);
+
+const ChartComponents = ({ data }) => {
+  const chartData = {
+    chart: {
+      caption: "Languages",
+      decimals: 0,
+      pieRadius: "45%",
+      theme: "fusion",
+    },
+    data,
+  };
+  const chartConfigs = {
+    type: "pie3d",
+    width: 400,
+    height: 400,
+    dataFormat: "json",
+    dataSource: chartData,
+  };
+
+  return <ReactFC {...chartConfigs} />;
 };
-
-export default Pie3D;
+export default ChartComponents;
